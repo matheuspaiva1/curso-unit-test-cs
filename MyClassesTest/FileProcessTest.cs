923d2fe4f889ed845bf4711ecad353343631650c
matheuspaiva1 <matheuspaiva0007@gmail.com>
2024-10-25 15:03:59 -0300

    executando todos testes da minha classe

---
+++ MyClassesTest/FileProcessTest.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyClasses;
+using System;
+
+namespace MyClassesTest
+{
+    [TestClass]
+    public class FileProcessTest
+    {
+        [TestMethod]
+        public void fileNameDoesExists()
+        {
+            FileProcess fp = new FileProcess();
+            bool fromCall;
+
+            fromCall = fp.FileExists(@"C:\Windows\Regedit.exe");
+
+            Assert.IsTrue(fromCall);
+        }
+
+        [TestMethod]
+        public void fileNameDoesNotExists()
+        {
+            FileProcess fp = new FileProcess();
+            bool fromCall;
+
+            fromCall = fp.FileExists(@"C:\Regedit.exe");
+
+            Assert.IsFalse(fromCall);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void fileNameNullOrEmpty_ThrowsArgumentNullException()
+        {
+            //TODO  
+            FileProcess fp = new FileProcess();
+
+            fp.FileExists("");
+        }
+
+        [TestMethod]
+        public void fileNameNullOrEmpty_ThrowsArgumentNullException_UsingTryCatch()
+        {
+            //TODO  
+            FileProcess fp = new FileProcess();
+
+            try
+            {
+                fp.FileExists("");
+            }
+            catch (ArgumentException)
+            {
+                // test sucess!
+                return;
+            }
+
+            Assert.Fail("Fail Excepted");
+        }
+    }
+}
