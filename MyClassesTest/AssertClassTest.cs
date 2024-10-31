5a2ec08e2f82ab1af0eac179c51a1d7e1087ab91
matheuspaiva1 <matheuspaiva0007@gmail.com>
2024-10-31 10:43:13 -0300

    AreEqual and AreNotEqual

---
+++ MyClassesTest/AssertClassTest.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace MyClassesTest
+{
+    [TestClass]
+    public class AssertClassTest
+    {
+        [TestMethod]
+        [Owner("Matheus")]
+        public void AreEqualTest()
+        {
+            string str1 = "Matheus";
+            string str2 = "Matheus";
+
+            Assert.AreEqual(str1, str2);
+        }
+
+        [TestMethod]
+        [Owner("Matheus")]
+        [ExpectedException(typeof(AssertFailedException))]
+        public void AreEqualCaseSensitiveTest()
+        {
+            string str1 = "Matheus";
+            string str2 = "matheus";
+
+            Assert.AreEqual(str1, str2, false);
+        }
+
+        [TestMethod]
+        [Owner("Matheus")]
+        public void AreNotEqualTest()
+        {
+            string str1 = "Matheus";
+            string str2 = "Thiago";
+
+            Assert.AreNotEqual(str1, str2);
+        }
+
+
+    }
+}
