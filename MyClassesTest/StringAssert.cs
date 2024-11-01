3687189baf722d1f14b7025560cf369984f95b0e
matheuspaiva1 <matheuspaiva0007@gmail.com>
2024-11-01 15:58:01 -0300

    String asserts

---
+++ MyClassesTest/StringAssert.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Text.RegularExpressions;
+
+namespace MyClassesTest
+{
+    [TestClass]
+    public class StringAssertClassTest
+    {
+        [TestMethod]
+        [Owner("Matheus")]
+        public void ContainsTest()
+        {
+            string str1 = "Matheus Paiva";
+            string str2 = "Paiva";
+
+            StringAssert.Contains(str1, str2);
+        }
+
+        [TestMethod]
+        [Owner("Matheus")]
+        public void StartsWithTest()
+        {
+            string str1 = "Todos Caixa Alta";
+            string str2 = "Todos Caixa";
+
+            StringAssert.StartsWith(str1, str2);
+        }
+
+        [TestMethod]
+        [Owner("Matheus")]
+        public void IsAllLowerCaseTest()
+        {
+            Regex reg = new Regex(@"^([^A-Z])+$");
+
+            StringAssert.Matches("todos caixa", reg);
+        }
+
+        [TestMethod]
+        [Owner("Matheus")]
+        public void IsNotAllLowerCaseTest()
+        {
+            Regex reg = new Regex(@"^([^A-Z])+$");
+
+            StringAssert.DoesNotMatch("Todos Caixa", reg);
+        }
+    }
+}
