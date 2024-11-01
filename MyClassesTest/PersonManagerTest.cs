dbb4c60180ba0859234718b93161a5473f3bdaed
matheuspaiva1 <matheuspaiva0007@gmail.com>
2024-11-01 16:47:07 -0300

    Code Coverage em Employee

---
+++ MyClassesTest/PersonManagerTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyClasses.PersonClasses;
+using System;
+
+namespace MyClassesTest
+{
+    [TestClass]
+    public class PersonManagerTest
+    {
+        [TestMethod]
+        public void CreatePerson_OfTypeEmployeeTest()
+        {
+            PersonManager PerMgr = new PersonManager();
+            Person per;
+
+            per = PerMgr.CreatePerson("Matheus", "Paiva", false);
+
+            Assert.IsInstanceOfType(per, typeof(Employee));
+        }
+    }
+}
